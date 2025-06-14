e400c3d520f22fa2c44e9d2ade92500b54d2a9cc
Mahami mathias <mathiasmahami@gmail.com>
2025-06-14 04:15:44 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -135,7 +135,7 @@ app.get('/', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
 
